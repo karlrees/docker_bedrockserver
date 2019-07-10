4a235174076e7ab2722717221ce859ec5500b73c
karlrees <38505961+karlrees@users.noreply.github.com>
2019-07-10 11:24:43 -0500

    Added ability to import permissions.json file
    Looks for worldname.permissions.json, where worldname is the name of your world.  Looks in the same folder as the server.properties file.

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 # if world folder does not exist, create it
 mkdir -p -- "${MCSERVERFOLDER}/worlds/${WORLD}"
 
-# if no existing custom properties file, copt template over
+# if no existing custom properties file, copy template over
 if ! [ -f "${MCSERVERFOLDER}/worlds/${WORLD}.properties" ]; then
  mv "${MCSERVERFOLDER}/server.properties.template" "/${MCSERVERFOLDER}/worlds/${WORLD}.properties"
 fi
@@ -11,8 +11,9 @@ fi
 # fix custom properties file world location
 sed -i -e "s/=world/=$WORLD/g" "${MCSERVERFOLDER}/worlds/${WORLD}.properties"
 
-# copy custom properties file to correct location
+# copy custom properties and permissions files to correct location
 cp "${MCSERVERFOLDER}/worlds/${WORLD}.properties" "${MCSERVERFOLDER}/server.properties"
+cp "${MCSERVERFOLDER}/worlds/${WORLD}.permissions.json" "${MCSERVERFOLDER}/permissions.json"
 
 # change default server port
 sed -i -e "s/=19132/=$MCPORT/g" "${MCSERVERFOLDER}/server.properties"
