a24a2b53552255ea60e196d129c3270077293507
karlrees <github@karlrees.com>
2019-11-13 23:11:13 -0600

    Add support for multiple servers using new file structure

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM ubuntu
 ENV container docker
 
 # set minecraft world name and port and apk filename
-ENV WORLD='default'
+ENV WORLD='world'
 ENV MCPORT=19132
 ARG INSTALLERURL=https://minecraft.azureedge.net/bin-linux/bedrock-server-1.13.0.34.zip
 ENV MCSERVERFOLDER=/srv/bedrockserver
 ENV MCVOLUME=/config
+ENV MCUSER=1001
+ENV MCGROUP=0
 
 EXPOSE $MCPORT
 
@@ -22,24 +24,21 @@ RUN curl $INSTALLERURL --output mc.zip && \
   unzip mc.zip -d $MCSERVERFOLDER && \
   rm mc.zip && \
   mkdir $MCSERVERFOLDER/default $MCVOLUME && \
-  chown -Rf 1001:0 $MCSERVERFOLDER $MCVOLUME && \
+  chown -Rf $MCUSER:$MCGROUP $MCSERVERFOLDER $MCVOLUME && \
   chmod -Rf g=u $MCSERVERFOLDER $MCVOLUME && \
   rm ${MCSERVERFOLDER}/server.properties && \
   for i in permissions.json whitelist.json behavior_packs definitions resource_packs structures;do mv $MCSERVERFOLDER/$i $MCSERVERFOLDER/default/$i;done
 
-
-# create folders for minecraft resources
+# create folder for minecraft resources
 VOLUME $MCVOLUME
-
-# copy over server properties template
-# COPY server.properties.template $MCSERVERFOLDER/server.properties.template
 
 # set up startup script
 COPY startup.sh /srv/bedrockserver/
 RUN chmod +x $MCSERVERFOLDER/startup.sh && \
   chmod -Rf g=u $MCSERVERFOLDER/startup.sh && \
-  chown 1001:0 $MCSERVERFOLDER/startup.sh
+  chown $MCUSER:$MCGROUP $MCSERVERFOLDER/startup.sh
 
-USER 1001
+# set default user to minecraft user
+USER $MCUSER:$MCGROUP
 
 CMD ["/srv/bedrockserver/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -6,74 +6,102 @@ then
  mkdir -p -- "${MCVOLUME}/worlds/${WORLD}"
 fi
 
-echo "Generating server configuration:"
-# create base for server.properties
-echo -e "server-port=19132\nlevel-name=world" > ${MCSERVERFOLDER}/server.properties
-echo -e "\tserver-port=19132\n\tlevel-name=world"
-# Parse all environment variables beginning with MCPROP to generate server.properties
-# For each matching line
-#  - Get property name from beggining to first = sign
-#    - Remove MCPROP_ from beginning
-#    - Switch to lowercase
-#    - Convert _ to -
-#  - Get property value from everything after first = sign
-# Examples
-#  - MCPROP_ALLOW_CHEATS=true
-#    allow-cheats=true
-for P in `printenv | grep '^MCPROP_'`
-do
-	PROP_NAME=${P%%=*}
-  PROP_VALUE=${P##${PROP_NAME}=}
-	PROP_NAME=${PROP_NAME#*_}
-	PROP_NAME=`echo ${PROP_NAME} | tr '[:upper:]' '[:lower:]'`
-	PROP_NAME=`echo ${PROP_NAME} | tr "_" "-"`
-  echo -e "\t${PROP_NAME}=${PROP_VALUE}"
-	echo "${PROP_NAME}=${PROP_VALUE}" >> ${MCSERVERFOLDER}/server.properties
-done
+if [ -f "${MCVOLUME}/${WORLD}.server.properties" ]
+then
+	ln -s ${MCVOLUME}/${WORLD}.server.properties ${MCSERVERFOLDER}/server.properties
+else
+	echo "Generating server configuration:"
+	# create base for server.properties
+	echo -e "server-port=19132\nlevel-name=${WORLD}" > ${MCSERVERFOLDER}/server.properties
+	# Parse all environment variables beginning with MCPROP to generate server.properties
+	# For each matching line
+	#  - Get property name from beggining to first = sign
+	#    - Remove MCPROP_ from beginning
+	#    - Switch to lowercase
+	#    - Convert _ to -
+	#  - Get property value from everything after first = sign
+	# Examples
+	#  - MCPROP_ALLOW_CHEATS=true
+	#    allow-cheats=true
+	for P in `printenv | grep '^MCPROP_'`
+	do
+		PROP_NAME=${P%%=*}
+	  PROP_VALUE=${P##${PROP_NAME}=}
+		PROP_NAME=${PROP_NAME#*_}
+		PROP_NAME=`echo ${PROP_NAME} | tr '[:upper:]' '[:lower:]'`
+		PROP_NAME=`echo ${PROP_NAME} | tr "_" "-"`
+	  echo -e "\t${PROP_NAME}=${PROP_VALUE}"
+		echo "${PROP_NAME}=${PROP_VALUE}" >> ${MCSERVERFOLDER}/server.properties
+	done
+fi
 
 # Link permission and whilelist
 for f in permissions.json whitelist.json
 do
-  # If file doesn't exist create from minecraft default
-	if ! [ -f "${MCVOLUME}/${f}" ]
+	# If world.filename is found, link to that	
+	if [ -f "${MCVOLUME}/${WORLD}.${f}" ]
 	then
-		cp ${MCSERVERFOLDER}/default/${f} ${MCVOLUME}/${f}
+		ln -s ${MCVOLUME}/${WORLD}.${f} ${MCSERVERFOLDER}/${f}
+	else
+	  	# If file doesn't exist create from minecraft default
+		if ! [ -f "${MCVOLUME}/${f}" ]
+		then
+			cp ${MCSERVERFOLDER}/default/${f} ${MCVOLUME}/${f}
+		fi
+		ln -s ${MCVOLUME}/${f} ${MCSERVERFOLDER}/${f}
 	fi
-	ln -s ${MCVOLUME}/${f} ${MCSERVERFOLDER}/${f}
 done
 
 # Link Debug_Log and valid_known_packs
 for f in Debug_Log.txt valid_known_packs.json
 do
-  # If file doesn't exist create empty
-	if ! [ -f "${MCVOLUME}/${f}" ]
+	# If world.filename is found, link to that
+	if [ -f "${MCVOLUME}/${WORLD}.${f}" ]
 	then
-		touch ${MCVOLUME}/${f}
+		ln -s ${MCVOLUME}/${WORLD}.${f} ${MCSERVERFOLDER}/${f}
+	else
+	  	# If file doesn't exist create empty
+		if ! [ -f "${MCVOLUME}/${f}" ]
+		then
+			touch ${MCVOLUME}/${f}
+		fi
+		ln -s ${MCVOLUME}/${f} ${MCSERVERFOLDER}/${f}
 	fi
-	ln -s ${MCVOLUME}/${f} ${MCSERVERFOLDER}/${f}
 done
 
 # Link directories with defaults
 for d in behavior_packs definitions resource_packs structures worlds
 do
-  # if directory doesn't exist create from minecraft default
-	if ! [ -d "${MCVOLUME}/${d}" ]
+	# If world.directory is found, link to that
+	if [ -f "${MCVOLUME}/${WORLD}.${d}" ]
 	then
-		cp -a ${MCSERVERFOLDER}/default/${d} ${MCVOLUME}/${d}
+		ln -s ${MCVOLUME}/${WORLD}.${d} ${MCSERVERFOLDER}/${d}
+	else
+	  	# if directory doesn't exist create from minecraft default
+		if ! [ -d "${MCVOLUME}/${d}" ]
+		then
+			cp -a ${MCSERVERFOLDER}/default/${d} ${MCVOLUME}/${d}
+		fi
+		ln -s ${MCVOLUME}/${d} ${MCSERVERFOLDER}/${d}
 	fi
-	ln -s ${MCVOLUME}/${d} ${MCSERVERFOLDER}/${d}
 done
 
 # Link directories without defaults
-for d in development_behavior_packs development_resource_packs premium_cache treamtemts world_templates
+for d in development_behavior_packs development_resource_packs premium_cache treatments world_templates
 do
-  # if diretory doesn't exist create empty
-	if ! [ -d "${MCVOLUME}/${d}" ]
+	# If world.directory is found, link to that
+	if [ -f "${MCVOLUME}/${WORLD}.${d}" ]
 	then
-		mkdir ${MCVOLUME}/${d}
-		chmod g=u ${MCVOLUME}/${d}
+		ln -s ${MCVOLUME}/${WORLD}.${d} ${MCSERVERFOLDER}/${d}
+	else
+	  	# if directory doesn't exist create empty
+		if ! [ -d "${MCVOLUME}/${d}" ]
+		then
+			mkdir ${MCVOLUME}/${d}
+			chmod g=u ${MCVOLUME}/${d}
+		fi
+		ln -s ${MCVOLUME}/${d} ${MCSERVERFOLDER}/${d}
 	fi
-	ln -s ${MCVOLUME}/${d} ${MCSERVERFOLDER}/${d}
 done
 
 echo "STARTING BEDROCKSERVER: ${WORLD} on ${HOSTNAME}:${MCPORT} ..."
